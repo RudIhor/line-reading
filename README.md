810ef008015db1bb0da982afad3d3a941a56a0ee
Ihor Rud <Igorryd7@gmail.com>
2025-02-23 10:35:26 +0200

    feat: release 1.0.0 version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Line Reading
+
+Speed up your reading by 2x to 3x with this simple red line.
---
+++ content.js
@@ -0,0 +1,1 @@
+document.body.style.cursor = `url(${chrome.runtime.getURL('images/cursor.png')}), auto`;
